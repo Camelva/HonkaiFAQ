ceb29e0c958e3acfd2124114d1ea6ba9f6d29833
Camelva <Camelva@users.noreply.github.com>
2022-12-04 12:19:56 +0000

    deploy: 03791c4dc44ed1fdb03090b54ad3392bc5f83d09

--- assets/faq_48.md.96f6d3bd.js
+++
@@ -1,6 +0,0 @@
-import{_ as a,c as s,o as n,a as e}from"./app.98b259d4.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"faq/48.md","lastUpdated":1670122480000}'),t={name:"faq/48.md"},l=e(`<p>##Growth Fund tags: growth fund</p><div class="language-toml"><button title="Copy Code" class="copy"></button><span class="lang">toml</span><pre class="shiki"><code><span class="line"><span style="color:#89DDFF;">[</span><span style="color:#FFCB6B;">Growth</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">Fund</span><span style="color:#89DDFF;">]</span></span>
-<span class="line"><span style="color:#A6ACCD;">Captains level 6+ can purchase.</span></span>
-<span class="line"><span style="color:#A6ACCD;">Captains that are above the level of the perks can automatically claim them. </span></span>
-<span class="line"></span>
-<span class="line"><span style="color:#A6ACCD;">Ex. CL 80 can purchase a Growth Fund and claim all of the gold/crystals.</span></span>
-<span class="line"></span></code></pre></div>`,2),o=[l];function p(c,r,i,_,d,h){return n(),s("div",null,o)}const m=a(t,[["render",p]]);export{u as __pageData,m as default};
